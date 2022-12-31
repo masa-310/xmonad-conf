1f53843d53ef03b24a0ca7d9ceaf56f4cbc1bded
Masashi Sato <gmasa.310@gmail.com>
2023-01-01 07:19:30 +0900

    refactor

--- app/xmonad.hs
+++ app/xmonad.hs
@@ -1,155 +1,105 @@
+import Config (getFont, getModMask, getProjects, getPromptConf, getTerminal, mkConfigByHost)
+import Control.Concurrent (threadDelay)
 import Control.Monad
-import Data.Time.Format (formatTime, defaultTimeLocale)
+import Data.Maybe (fromMaybe)
 import Data.Time (getZonedTime)
-
-import XMonad -- additionalKeysP
-import XMonad.Util.EZConfig (additionalKeys)
-
--- layout
-import XMonad.Layout (Full )
-import XMonad.Layout.ResizableTile (ResizableTall(ResizableTall))
-import XMonad.Layout.ThreeColumns (ThreeCol(ThreeColMid))
-import XMonad.Layout.TwoPane (TwoPane(TwoPane))
-import XMonad.Layout.NoBorders (noBorders)
-import XMonad.Layout.ToggleLayouts (toggleLayouts)
-import XMonad.Layout.Spacing (spacingRaw, Border(Border))
-import XMonad.Layout.Gaps
-import XMonad.Layout.WorkspaceDir (workspaceDir, changeDir)
-import XMonad.Layout.Drawer (onLeft, simpleDrawer)
-import XMonad.Layout.Dishes (Dishes(Dishes))
+import Data.Time.Format (defaultTimeLocale, formatTime)
+import Polybar (polybar)
+import System.Environment (lookupEnv)
+import XMonad
+import XMonad.Actions.DynamicProjects (Project (..), dynamicProjects, shiftToProjectPrompt, switchProject, switchProjectPrompt)
+import XMonad.Actions.DynamicWorkspaces (removeWorkspaceByTag)
+import XMonad.Actions.Search
+import XMonad.Config.Desktop (desktopConfig)
+import XMonad.Hooks.EwmhDesktops (addEwmhWorkspaceSort, ewmh, ewmhFullscreen)
+import XMonad.Hooks.ManageDocks (avoidStruts, docks)
+import XMonad.Hooks.StatusBar (withSB)
+import XMonad.Layout (Full)
+import XMonad.Layout.Dishes (Dishes (Dishes))
 import XMonad.Layout.DragPane
-import XMonad.Layout.Maximize (maximizeWithPadding, maximizeRestore)
-
--- Prompt
+import XMonad.Layout.Drawer (onLeft, simpleDrawer)
+import XMonad.Layout.Gaps
+import XMonad.Layout.Maximize (maximizeRestore, maximizeWithPadding)
+import XMonad.Layout.NoBorders (noBorders)
+import XMonad.Layout.ResizableTile (ResizableTall (ResizableTall))
+import XMonad.Layout.Spacing (Border (Border), spacingRaw)
+import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
+import XMonad.Layout.ToggleLayouts (toggleLayouts)
+import XMonad.Layout.TwoPane (TwoPane (TwoPane))
+import XMonad.Layout.WorkspaceDir (changeDir, workspaceDir)
 import XMonad.Prompt
 import XMonad.Prompt.AppLauncher (launchApp)
 import XMonad.Prompt.AppendFile
 import XMonad.Prompt.FuzzyMatch (fuzzyMatch)
-
--- Action
-import XMonad.Actions.Search
-import XMonad.Actions.DynamicProjects (Project(..), dynamicProjects, switchProjectPrompt, shiftToProjectPrompt, switchProject)
-import XMonad.Actions.DynamicWorkspaces (removeWorkspaceByTag)
-
-import XMonad.Hooks.ManageDocks (avoidStruts, docks)
-
+import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Replace (replace)
-import XMonad.Util.WindowProperties (Property(ClassName, Or))
+import XMonad.Util.WindowProperties (Property (ClassName, Or))
 import XMonad.Util.WorkspaceCompare (filterOutWs)
 
-
-import XMonad.Config.Desktop (desktopConfig)
-
-import Control.Concurrent (threadDelay)
-
-import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen, addEwmhWorkspaceSort)
-import XMonad.Hooks.StatusBar (withSB)
-
-import Polybar (polybar)
-
-main :: IO()
-
 gap_ = 0
+
 borderWidth_ = 0
 
-terminal_ = "lxterminal"
-myfont="xft:DejaVu Sans Mono:alias=true:size=11:hinting=true,xft:Symbola,xft:Noto Color Emoji"
-
-modMask_ = mod4Mask
 -- workspaces_ = map show [1..9]
-workspaces_ =  []
-
+workspaces_ = []
 
 layoutHook_ =
-  workspaceDir "~"
-    $ avoidStruts
-    $ toggleLayouts (noBorders Full)
-    $ spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True
-    $ maximizeWithPadding 10 (ResizableTall 1 (3/100) (3/5) []
--- can use something useful?
---  $ drawer `onLeft` (ResizableTall 1 (3/100) (3/5) [])
---  ||| Dishes 2 (1/6)
---  ||| dragPane Horizontal 0.1 0.5
-      ||| ThreeColMid 1 (3/100) (1/2)
-      ||| TwoPane (3/100) (3/5)
-      ||| Full)
-  where drawer = simpleDrawer 0.01 0.3 (ClassName "Rhythmbox" `Or` ClassName "Xchat")
-
-startupHook_ = do
-  spawn "autorandr default"
-  spawn "feh --bg-fill .wallpaper/wallpaper.png"
-  spawn "wmname LG3D"
-  switchProject $ head projects
-  removeWorkspaceByTag "1"
-  removeWorkspaceByTag "2"
-
-promptConf = def
-    { promptBorderWidth = 1
-    , alwaysHighlight = True
-    , height = 22
-    , historySize = 256
-    , font = myfont
-    -- , bgColor = myBackgroundColor
-    -- , fgColor = myContentColor
-    -- , bgHLight = myBackgroundColor
-    -- , fgHLight = myContentColor
-    -- , borderColor = myBackgroundColor
-    , position = Bottom
-    , autoComplete = Just 100
-    , showCompletionOnTab = False
-    , searchPredicate = fuzzyMatch
-    , defaultPrompter = id
-    , sorter = const id
-    , maxComplRows = Just 7
-    , promptKeymap = defaultXPKeymap
-    , completionKey = (0, xK_Tab)
-    , changeModeKey = xK_grave
-    , historyFilter = id
-    , defaultText = []
-    }
+  workspaceDir "~" $
+    avoidStruts $
+      toggleLayouts (noBorders Full) $
+        spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True $
+          maximizeWithPadding
+            10
+            ( ResizableTall 1 (3 / 100) (3 / 5) []
+                -- can use something useful?
+                --  $ drawer `onLeft` (ResizableTall 1 (3/100) (3/5) [])
+                --  ||| Dishes 2 (1/6)
+                --  ||| dragPane Horizontal 0.1 0.5
+                ||| ThreeColMid 1 (3 / 100) (1 / 2)
+                ||| TwoPane (3 / 100) (3 / 5)
+                ||| Full
+            )
+  where
+    drawer = simpleDrawer 0.01 0.3 (ClassName "Rhythmbox" `Or` ClassName "Xchat")
 
 googleChrome = "/usr/bin/google-chrome-stable"
 
-projects :: [Project]
-projects = [
-  Project {
-    projectName = "home"
-    , projectDirectory = "~"
-    , projectStartHook = Nothing
-    }
-  , Project {
-    projectName = "snd_screen"
-    , projectDirectory = "~"
-    , projectStartHook = Just $ do
-        spawn "slack"
-        spawn "obsidian"
-    }
-  , Project {
-    projectName = "tmp"
-    , projectDirectory = "~"
-    , projectStartHook = Nothing
-    }
-  ]
-
+main :: IO ()
 main = do
+  conf <- maybe def mkConfigByHost <$> lookupEnv "XMONAD_HOST"
+  let terminal = getTerminal conf
+      projects = getProjects conf
+      promptConf = getPromptConf conf
+      font = getFont conf
+      modMask = getModMask conf
+      startupHook_ = do
+        spawn "autorandr default"
+        spawn "feh --bg-fill .wallpaper/wallpaper.png"
+        spawn "wmname LG3D"
+        switchProject $ head projects
+        removeWorkspaceByTag "1"
+        removeWorkspaceByTag "2"
   replace
-  xmonad . withSB polybar . addEwmhWorkspaceSort (pure $ filterOutWs [""]) . ewmhFullscreen . ewmh . docks $ dynamicProjects projects $ def {
-      terminal        = terminal_
-      , modMask       = modMask_
-      , layoutHook    = layoutHook_
-      , startupHook   = startupHook_
-      , workspaces = workspaces_
-    } `additionalKeys` [
-    ((modMask_, xK_c), changeDir promptConf ) , ((modMask_, xK_f), withFocused (sendMessage . maximizeRestore))
-    -- , ((modMask_, xK_h), launchApp def "feh" )
-    -- , ((modMask_, xK_e), launchApp promptConf "evince" )
-    , ((modMask_, xK_n), do 
-        date <- io $ fmap (formatTime defaultTimeLocale "[%Y-%m-%d %H:%M] ") getZonedTime
-        appendFilePrompt' promptConf (date ++)  "~/NOTES"
-    )
-    , ((modMask_, xK_g), promptSearchBrowser (greenXPConfig { font = myfont }) googleChrome google)
-    , ((modMask_, xK_d), spawn "dmenu_run")
-    , ((modMask_, xK_space), switchProjectPrompt promptConf)
-    , ((modMask_, xK_slash), shiftToProjectPrompt promptConf)
-
-    ]
+  xmonad . withSB polybar . addEwmhWorkspaceSort (pure $ filterOutWs [""]) . ewmhFullscreen . ewmh . docks $
+    dynamicProjects projects $
+      def
+        { terminal = terminal,
+          modMask = modMask,
+          layoutHook = layoutHook_,
+          startupHook = startupHook_,
+          workspaces = workspaces_
+        }
+        `additionalKeys` [ ((modMask, xK_c), changeDir promptConf),
+                           ((modMask, xK_f), withFocused (sendMessage . maximizeRestore)),
+                           -- , ((modMask_, xK_h), launchApp def "feh" )
+                           -- , ((modMask_, xK_e), launchApp promptConf "evince" )
+                           ( (modMask, xK_n),
+                             do
+                               date <- io $ fmap (formatTime defaultTimeLocale "[%Y-%m-%d %H:%M] ") getZonedTime
+                               appendFilePrompt' promptConf (date ++) "~/NOTES"
+                           ),
+                           ((modMask, xK_g), promptSearchBrowser (greenXPConfig {font = font}) googleChrome google),
+                           ((modMask, xK_d), spawn "dmenu_run"),
+                           ((modMask, xK_space), switchProjectPrompt promptConf),
+                           ((modMask, xK_slash), shiftToProjectPrompt promptConf)
+                         ]
---
+++ src/Config.hs
@@ -0,0 +1,83 @@
+module Config (mkConfigByHost, getTerminal, getPromptConf, getProjects, getFont, getModMask) where
+
+import Data.Default.Class
+import XMonad (KeyMask, mod4Mask, xK_Tab, xK_grave)
+import XMonad.Actions.DynamicProjects (Project (..))
+import XMonad.Prompt as Prompt
+import XMonad.Prompt.FuzzyMatch (fuzzyMatch)
+
+type HostName = String
+
+data Config = Config
+  { terminal :: String,
+    font :: String,
+    projects :: [Project],
+    promptConfig :: XPConfig,
+    modMask :: KeyMask
+  }
+
+defaultFont = "xft:DejaVu Sans Mono:alias=true:size=11:hinting=true,xft:Symbola,xft:Noto Color Emoji"
+
+ninjinConfig :: Config
+ninjinConfig = def
+
+instance Default Config where
+  def =
+    Config
+      { terminal = "lxterminal",
+        Config.font = defaultFont,
+        projects =
+          [ Project
+              { projectName = "home",
+                projectDirectory = "~",
+                projectStartHook = Nothing
+              }
+          ],
+        promptConfig =
+          def
+            { promptBorderWidth = 1,
+              alwaysHighlight = True,
+              height = 22,
+              historySize = 256,
+              Prompt.font = defaultFont,
+              -- , bgColor = myBackgroundColor
+              -- , fgColor = myContentColor
+              -- , bgHLight = myBackgroundColor
+              -- , fgHLight = myContentColor
+              -- , borderColor = myBackgroundColor
+              position = Bottom,
+              autoComplete = Just 100,
+              showCompletionOnTab = False,
+              searchPredicate = fuzzyMatch,
+              defaultPrompter = id,
+              sorter = const id,
+              maxComplRows = Just 7,
+              promptKeymap = defaultXPKeymap,
+              completionKey = (0, xK_Tab),
+              changeModeKey = xK_grave,
+              historyFilter = id,
+              defaultText = []
+            },
+        modMask = mod4Mask
+      }
+
+mkConfigByHost :: HostName -> Config
+mkConfigByHost hostName =
+  case hostName of
+    "ninjin" -> ninjinConfig
+    _ -> ninjinConfig
+
+getTerminal :: Config -> String
+getTerminal = terminal
+
+getPromptConf :: Config -> XPConfig
+getPromptConf = promptConfig
+
+getFont :: Config -> String
+getFont = Config.font
+
+getProjects :: Config -> [Project]
+getProjects = projects
+
+getModMask :: Config -> KeyMask
+getModMask = modMask
