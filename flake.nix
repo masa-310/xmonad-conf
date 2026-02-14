3fca646dea55b9f176128f1df0650eca82d7f149
Masashi SATO <gmasa.310@gmail.com>
2026-02-14 22:07:18 +0900

    📦 build: overhaul Nix and Stack build setup
    - Refactored Nix flake for Stack integration, GHC 9.10.3, and dev tools.
    - Updated `nixpkgs` to fix a Stack bug, and bumped `xmonad` and `lts-24.30`.
    - Added `hie.yaml` for HLS and removed `Setup.hs`, `test/Spec.hs`.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,16 @@
 {
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  # a dependency of Stack, that does not have this bug: https://github.com/commercialhaskell/rio/issues/264
+  # inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=pull/466258/head";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      haskellNix,
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -11,38 +19,87 @@
         #"aarch64-darwin"
       ];
     in
-      flake-utils.lib.eachSystem supportedSystems (system:
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
       let
-        overlays = [ haskellNix.overlay
+        overlays = [
+          haskellNix.overlay
           (final: prev: {
-            xmonad-config-project =
-              final.haskell-nix.project {
-                src = ./.;
-                  #evalSystem = "x86_64-linux";
-              };
+            xmonad-config-project = final.haskell-nix.project {
+              src = ./.;
+              #evalSystem = "x86_64-linux";
+            };
           })
         ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-          #hls = pkgs.haskell-language-server.override { supportedGhcVersions = [ "925" ]; };
-        flake = pkgs.xmonad-config-project.flake {};
-      in flake // {
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        # should be matched with stack ghc version
+        hPkgs = pkgs.haskell.packages."ghc9103";
+        # Wrap Stack to work with our Nix integration. We do not want to modify
+        # stack.yaml so non-Nix users do not notice anything.
+        # - no-nix: We do not want Stack's way of integrating Nix.
+        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+        # --no-install-ghc  # Do not try to install GHC if no matching GHC found on PATH
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack"; # will be available as the usual `stack` in terminal
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+        #hls = pkgs.haskell-language-server.override { supportedGhcVersions = [ "925" ]; };
+        flake = pkgs.xmonad-config-project.flake { };
+        devTools = [
+          hPkgs.ghc
+          hPkgs.haskell-language-server
+          hPkgs.implicit-hie
+          stack-wrapped
+        ];
+      in
+      flake
+      // {
         defaultPackage = flake.packages."xmonad-config:exe:xmonad-config";
         packages.xmonad-config = flake.packages."xmonad-config:exe:xmonad-config";
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            stack
-                #hls
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            autoconf
+            gcc
+            zlib
+            xorg.libX11
+            xorg.libXext
+            xorg.libXft
+            xorg.libXinerama
+            xorg.libXpm
+            xorg.libXrandr
+            xorg.libXrender
+            xorg.libXScrnSaver
+            libxcb
+            libxdmcp
+            expat
           ];
+          buildInputs = devTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devTools;
         };
-      });
+      }
+    );
 
   # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
     # This sets the flake to use the IOG nix cache.
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
     allow-import-from-derivation = "true";
   };
 }
