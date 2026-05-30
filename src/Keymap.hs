2e08ca11f8d458808305d899649a7ed112de9c69
Masashi SATO <gmasa.310@gmail.com>
2026-05-31 00:16:01 +0900

    ✨ feat(keymap): add volume keybindings with notifications
    Replaces old volume control keybindings.
    Displays volume changes via dunstify notifications.

--- src/Keymap.hs
+++ src/Keymap.hs
@@ -34,11 +34,11 @@ newKeyBindings myConf =
         ((modMask , xK_4), spawn "maim -s | xclip -selection clipboard -t image/png"),
         ((modMask .|. shiftMask, xK_4), spawn "maim -s | xclip -selection clipboard -t image/png && drawing -c"),
         ((modMask, xK_5), spawn "peek"),
+        ((modMask, xK_F1), spawn "dunstify -h string:x-canonical-private-synchronous:audio \"Volume: \" -h int:value:\"`pamixer -t && pamixer --get-volume`\"")
+        ((modMask, xK_F2), spawn "dunstify -h string:x-canonical-private-synchronous:audio \"Volume: \" -h int:value:\"`pamixer -d 5 && pamixer --get-volume`\""),
+        ((modMask, xK_F3), spawn "dunstify -h string:x-canonical-private-synchronous:audio \"Volume: \" -h int:value:\"`pamixer -i 5 && pamixer --get-volume`\""),
         ((modMask, xK_F5), spawn "brightnessctl set 10%-"),
         ((modMask, xK_F6), spawn "brightnessctl set 10%+"),
-        ((modMask, xK_7), spawn "pamixer -d 5"),
-        ((modMask, xK_8), spawn "pamixer -i 5"),
-        ((modMask, xK_9), spawn "pamixer -t")
       ]
 
 removalKeyBindings :: MyConfig -> [(ButtonMask, KeySym)]
